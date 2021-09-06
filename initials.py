5b2d7e12c106c4e1f9b33e5908166b567b24fd2e
Lore <jshaw93@gmail.com>
2021-09-06 14:07:15 -0600

    Create initials.py
    Homework Assignment 1 due 9/5/21

---
+++ initials.py
@@ -0,0 +1,21 @@
+# initials.py
+# James Shaw
+# 8/28/21
+
+# Displays initials in block format using a Multi-line String and Print function
+# Multi-line strings are a type of string that can, as the name suggests, use multiple lines.
+# These are useful for formatting strings in a way that is easier and faster to code than doing multiple print
+# methods or have a massive single line using newline characters "\n" to create newlines.
+# They are also used to give developers context on where to use certain bits of code that I hope we'll cover
+# later in the semester.
+
+print(""" JJJJJJJJJJJ    SSSSSSSS
+ JJJJJJJJJJJ  SS
+ JJJJJJJJJJJ  SS
+     JJJ       SSS
+     JJJ         SSS
+     JJJ          SSS
+     JJJ            SSS
+     JJJ              SS
+ JJ  JJJ              SS
+  JJJJ        SSSSSSSS""")
